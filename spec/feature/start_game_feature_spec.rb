1cba587d8a5387397435a8394521d730527cdfe9
MarySalemme <marysalemme1@gmail.com>
2017-09-05 17:45:24 +0200

    Adding feature test for starting a new game

---
+++ spec/feature/start_game_feature_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature "User can start playing" do
+  context "user not signed in and on the homepage" do
+    it "should see 'start game'" do
+      BotResponse.create(description: "test", victory: true)
+      visit('/')
+      expect(page).to have_content('Hi Chatbot')
+      expect(page).to have_link('Start game')
+    end
+  end
+end
